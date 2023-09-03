8ff63317a45cbb3de0a1b35edded02f96d7c7d6c
Aidan Marzeotti <aidan.marzeotti1@gmail.com>
2023-09-03 16:52:32 -0400

    first commit

---
+++ WebScraper.py
@@ -0,0 +1,17 @@
+# Web scraper 
+# Finds the following information from bizbuysell.com
+"""
+1. company website
+2. owner name
+3. owner email
+4. owner phone #
+5. broker name
+6. broker phone #
+7. broker email
+"""
+
+# to implement: filter by company ARR 
+
+# input the bizbuysell page, pre-filtered by geographic location, industry, price range, revenue, etc...
+def getListingData():
+    print()
