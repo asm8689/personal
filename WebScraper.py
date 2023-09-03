6d868ffb0a723560040fa2fca724e49a7c3f3f72
Aidan Marzeotti <aidan.marzeotti1@gmail.com>
2023-09-03 18:45:59 -0400

    website updates

--- WebScraper.py
+++ WebScraper.py
@@ -1,14 +1,15 @@
 # Web scraper 
 # Finds the following information from bizbuysell.com
 """
-1. company website
-2. owner name
+1. listing url
+2. location
 3. sale price
-4. owner email
-5. owner phone #
-6. broker name
-7. broker phone #
-8. broker email
+4. owner name
+5. owner email
+6. owner phone #
+7. broker name
+8. broker phone #
+9. broker email
 """
 
 # to implement: filter by company ARR 
@@ -36,19 +37,16 @@ listings = soup.find_all("li", class_="listing-row")
 
 csv_file = open("business_listings.csv", "w", newline="", encoding="utf-8")
 csv_writer = csv.writer(csv_file)
-csv_writer.writerow(["Company URL", "Owner Name", "Sale Price", "Owner Phone", "Owner Email", "Broker Name", "Broker Email"])
+csv_writer.writerow(["Company URL", "location", "Owner Name", "Sale Price"])
 
 # Loop through each listing and extract the required information
 for listing in listings:
-    company_url = listing.find("a", class_="listing-row-link")["href"]
-    owner_name = listing.find("div", class_="listing-owner").get_text(strip=True)
-    sale_price = listing.find("div", class_="listing-price").get_text(strip=True)
-    owner_phone = listing.find("div", class_="listing-phone").get_text(strip=True)
-    owner_email = listing.find("div", class_="listing-email").get_text(strip=True)
-    broker_name = listing.find("div", class_="listing-broker-name").get_text(strip=True)
-    broker_email = listing.find("div", class_="listing-broker-email").get_text(strip=True)
-
-    csv_writer.writerow([company_url, owner_name, sale_price, owner_phone, owner_email, broker_name, broker_email])
+    company_url = listing.find("a", class_="diamond")["href"]
+    location = listing.find("p", class_="location")
+    sale_price = listing.find("p", class_="asking-price").get_text(strip=True)
+    # website updated owner and broker fields
+   
+    csv_writer.writerow([company_url, location, sale_price])
 
 csv_file.close()
 print("Data has been scraped and saved to business_listings.csv")
